9a91b0a5e5019c9da8e9da58ea3db5e2bca998cd
nzimm <nizi2734@colorado.edu>
2019-04-17 15:49:07 -0600

    write from urand to data files [WIP]

--- level2/generate_data_files.py
+++ level2/generate_data_files.py
@@ -2,12 +2,15 @@
 import random
 import os
 
-data_directory = "/home/nizi2734/ctf.git/level2/data"
+#data_directory = "/home/nizi2734/ctf.git/level2/data"
+data_directory = "/home/nzimm/git/ctf/level2/data"
 random_source = "/dev/urandom"
 
 with open(random_source, "rb") as URAND:
-#    line = FILE.read(10)
-#    print(line)
-#    with open('./test_file.dat', 'wb') as OUT_FILE:
-#        OUT_FILE.write(line)
-#    FILE.close()
+    #for i in range(0,10):
+    for i in range(0,1):
+        data_file_path = os.path.join(data_directory, "file" + str(i) + ".dat")
+        with open(data_file_path, "wb") as OUT_FILE:
+            OUT_FILE.write(URAND.read(1000))
+            OUT_FILE.close()
+    URAND.close()
