4981581b7cdf530ed6e7c998bf8299b338ab42da
nzimm <nizi2734@colorado.edu>
2019-04-19 14:05:49 -0600

    Add level2 core files

---
+++ level2/level2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PASSLEN 17
+
+int main()
+{
+    const char *passwd_file = "/ctf/level2/pass.txt";
+    char passwd[PASSLEN], guess[PASSLEN];
+    FILE *file;
+
+    // Read password from file
+    file = fopen(passwd_file, "r");
+    if (file) {
+        fgets(passwd, PASSLEN, file);
+    }
+
+    // Prompt user
+    printf("Password: ");
+    fgets(guess, PASSLEN, stdin);
+
+    // Check answer - both `fish` and `/usr/bin/fish` are valid
+    if ( strncmp(guess, passwd, PASSLEN) == 0 ) {
+        // Call levelup
+        char *levelup = "/usr/local/bin/levelup";
+        char *argv[] = { levelup, NULL};
+        char *envp[] = { NULL };
+        execve(levelup, argv, envp);
+    }
+    else {
+        printf("Hmm, that doesn't look quite right...\n");
+        return 1;
+    }
+    return 0;
+}
---
+++ level2/setup.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#
+# Correctly chmod, chown, and move files
+# Additionally, run any makefiles or generation scripts on the fly
+#
+
+# Level to be copied, chowned AND chmoded for SETGID
+LEVEL="level2"
+
+# Git directory that the files live under
+BUILD_DIR="/home/nizi2734/ctf.git/$LEVEL/"
+
+# Files to be copied and chowned only
+PASS_FILE="pass.txt"
+FILES="objective.md level2.c data/ $PASS_FILE"
+
+# Level directory (default to level)
+DIR="/ctf/$LEVEL"
+
+# User to chown the level files to (default to level)
+USER="$LEVEL"
+
+
+# Run the makefile
+make clean && make
+
+# Generate data files
+$BUILD_DIR/generate_data_files.py
+
+# Save password to file
+grep --no-filename --text --only-matching "[0-9a-f]\{16\}" data/* | tail -n 1 > $PASS_FILE
+
+# Ensure that the make exited successfuly
+if [ $? -eq 0 ]; then
+
+    # Copy main binary
+    echo "sudo cp -f $LEVEL $DIR/"
+    sudo cp -f $LEVEL $DIR/
+
+    # Copy additional files
+    for F in $FILES; do
+        echo "sudo cp -rf $BUILD_DIR/$F $DIR/"
+        sudo cp -rf $BUILD_DIR/$F $DIR/
+    done
+
+    # Chown files to levelN:levelN
+    echo "sudo chown $LEVEL:$LEVEL $DIR/*"
+    sudo chown $LEVEL:$LEVEL $DIR/*
+
+    # Set the SETGID bit on main binary
+    echo "sudo chmod 2755 $DIR/$LEVEL"
+    sudo chmod 2755 $DIR/$LEVEL
+
+    # Tighten read permissions on pass.txt
+    echo "sudo chmod 440 $DIR/$PASS_FILE"
+    sudo chmod 440 $DIR/$PASS_FILE
+    
+
+fi
+
+# Clean up
+make clean
